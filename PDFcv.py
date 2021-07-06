05093a11021976e6c22531f3aa5f8faf7d9fce36
SwallowT <tanliuy@163.com>
2021-07-06 10:13:09 +0800

    亿点细节

--- PDFcv.py
+++ PDFcv.py
@@ -9,10 +9,11 @@ import pyperclip  # 引入模块
 
 recent_value = ""
 tmp_value = ""  # 初始化（应该也可以没有这一行，感觉意义不大。但是对recent_value的初始化是必须的）
-E_pun = u',.!?:[]()<>"\''
-C_pun = u'，。！？：【】（）《》“‘'
+E_pun = u',.!?:;()"\''
+C_pun = u'，。！？：；（）“‘'
 tableE2C = {f: t for f, t in zip(E_pun, C_pun)}
 tableC2E = {t: f for t, f in zip(C_pun, E_pun)}
+
 
 def strQ2B(ustring):
     """全角转半角# https://www.cnblogs.com/kaituorensheng/p/3554571.html
@@ -48,27 +49,38 @@ def E_C_trans(string):
     """
     global E_pun, C_pun, tableE2C, tableC2E
     for ep in E_pun:
-        pat = "(?<=[^\\x00-\\xff])\\" + ep
+        pat = r"(?<=[^\x00-\xff])" + "\\" + ep
         string = re.sub(pat, tableE2C[ep], string)
     for cp in C_pun:
-        string = re.sub('(?<=[^\\x00-\\xff])' + cp, tableC2E[cp], string)
+        string = re.sub(r"(?<=[\x00-\xff])" + cp, tableC2E[cp], string)
     return string.translate(tableE2C)
+
+
+def details(string):
+    new1 = string.replace("", "，")
+    new2 = new1.replace("∙", ".")
+    new3 = new2.replace("- ", "")
+    return new3
 
 
 while True:
     # 获取windows活动窗口、最前窗口的标题 https://blog.csdn.net/shjsfx/article/details/106089331
-    if "福昕阅读器" in win32gui.GetWindowText(win32gui.GetForegroundWindow()):
+    winName = win32gui.GetWindowText(win32gui.GetForegroundWindow())
+    if ("福昕阅读器" in winName) or ("WPS" in winName) or ("知云" in winName):
         tmp_value = pyperclip.paste()  # 读取剪切板复制的内容
 
         try:
             if tmp_value != recent_value:  # 如果检测到剪切板内容有改动，那么就进入文本的修改
                 recent_value = tmp_value
-                out = re.sub(r"\s{2,}", " ", recent_value)  # 将文本的换行符去掉，变成一个空格
-                changed = re.sub(r'(?<=[^\\x00-\\xff]) (?=[^\\x00-\\xff])', '', out)  # 中文去空格
-                changed1 = strQ2B(changed)  # 全角转半角
 
-                pyperclip.copy(changed1)  # 将修改后的文本写入系统剪切板中
-                print("\n Value changed: %s" % str(changed))  # 输出已经去除换行符的文本
+                out = re.sub(r"\s{2,}", " ", recent_value)  # 将文本的换行符去掉，变成一个空格
+                our1 = re.sub(r'(?<=[^\x00-\xff])\s(?=[^\x00-\xff])', '', out)  # 中文去空格
+                out2 = details(our1)  # 细节
+                out3 = strQ2B(out2)  # 全角转半角
+                out4 = E_C_trans(out3)  # 中英文标点转换
+
+                pyperclip.copy(out4)  # 将修改后的文本写入系统剪切板中
+                print("\n Value changed: %s" % str(out4))  # 输出已经去除换行符的文本
             time.sleep(0.1)
         except KeyboardInterrupt:  # 如果有ctrl+c，那么就退出这个程序。  （不过好像并没有用。无伤大雅）
             break
